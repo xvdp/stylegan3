03b2fa5f99832d765a2888d5930239f8d4374b7b
xvdp <xvdpahlen@gmail.com>
2023-07-08 11:23:55 -0700

    automatically mount TORCH_EXTENSIONS_DIR and DNNLIB_CACHE_DIR if exist in localhost

--- dockerrun.sh
+++ dockerrun.sh
@@ -14,27 +14,42 @@
 # ./dockerrun.sh gen_images.py --outdir=out --trunc=1 --seeds=2 \
 #         --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-afhqv2-512x512.pkl
 
+# if $TORCH_EXTENSIONS_DIR and $DNNLIB_CACHE_DIR exist in local host. mount as volumes
 
-## ARGS ysed for local display
+## ARGS added for local display
 # -e DISPLAY=unix$DISPLAY    
 # -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw -v /tmp/.X11-unix:/tmp/.X11-unix  -e XAUTHORITY=/tmp/.docker.xauth  # shares X11 xauth and sets environment
 # -v /dev:/dev  #       shares devices folder with docker to alow libGL load device info
 
-TAG=stylegan3_user
+TAG=stylegan3
 
-# run with gl capabilities, bash (if nor args) or visualizer.py
+# if environments exported - maintains relations... 
+ENVS=()
+if [ $TORCH_EXTENSIONS_DIR ]; then
+    MOUNT="/home/$(basename ${TORCH_EXTENSIONS_DIR})"
+    ENVS+=(-v ${TORCH_EXTENSIONS_DIR}:${MOUNT} -e TORCH_EXTENSIONS_DIR=${MOUNT}) ;
+fi
+if [ $DNNLIB_CACHE_DIR ]; then
+    MOUNT="/home/$(basename ${DNNLIB_CACHE_DIR})"
+    ENVS+=(-v ${DNNLIB_CACHE_DIR}:${MOUNT} -e DNNLIB_CACHE_DIR=${MOUNT}) ;
+fi
+
+WGL=()
+
+#  default arg /bin.bash
+if [ $# -gt 0 ]; then
+    args="$@"
+else
+    args=/bin/bash
+fi
+
+
 if [[ ($# -gt 1 && $2 == "visualizer.py") || $# == 0 ]]; then
-    if [ $# -gt 0 ]; then
-        args="$@"
-    else
-        args=/bin/bash
-    fi
+    # run with gl capabilities, bash (if nor args) or visualizer.py
     # share local host display and Xauthority : linux only, localhost docker only -
     touch /tmp/.docker.xauth && xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
-
-    echo "docker run --gpus all -it --rm --user $(id -u):$(id -g) -e DISPLAY=unix${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw -e XAUTHORITY=/tmp/.docker.xauth -v /dev:/dev -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch $TAG ${args}"
-    docker run --gpus all -it --rm --user $(id -u):$(id -g) -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw -e XAUTHORITY=/tmp/.docker.xauth -v /dev:/dev -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch $TAG $args
-else
-    # run without GL capabilites
-    docker run --gpus all -it --rm --user $(id -u):$(id -g) -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch $TAG $@
+    WGL=(-e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw -e XAUTHORITY=/tmp/.docker.xauth -v /dev:/dev )
 fi
+
+echo docker run --gpus all -it --rm --user $(id -u):$(id -g) "${WGL[@]}" -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch "${ENVS[@]}" $TAG $args
+docker run --gpus all -it --rm --user $(id -u):$(id -g) "${WGL[@]}" -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch "${ENVS[@]}" $TAG $args
